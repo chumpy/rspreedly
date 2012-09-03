28dd019c24d583a81299896f07147b5f0bd66876
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2012-09-03 14:06:35 -0700

    adding ability for line items to be one-time or recurring

--- lib/rspreedly/line_item.rb
+++ lib/rspreedly/line_item.rb
@@ -1,5 +1,5 @@
 module RSpreedly
   class LineItem < Base
-    attr_accessor :price, :notes, :amount, :description, :currency_code
+    attr_accessor :price, :notes, :amount, :description, :currency_code, :one_time
   end
 end
